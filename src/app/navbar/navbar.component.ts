8b4b9db5927dff21eaeb269b1f181b3b0f49c9ef
Josef Shainskiy <josef2011@gmail.com>
2018-10-01 22:17:19 +0300

    dgdg

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {  AuthenticationService, UserService } from "../_services";
 import { User } from "../_models";
+import { environment } from "../../environments/environment";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  webUrl=environment.webUrl;
   currentUser: User;
   constructor( private authenticationService:AuthenticationService,
     private userService:UserService
